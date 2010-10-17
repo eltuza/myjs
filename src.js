ce711bbc514943a864e0c2ee3c7ce446c2035943
martin <eltuza@gmail.com>
2010-10-17 17:20:39 -0300

    src js

---
+++ src.js
@@ -0,0 +1,10 @@
+//23*234/234
+//234*4567+3563.1234
+//1234*456//345234
+//234*234*234+
+//2452345 
+//3+4+'2354'+1234
+//234+"2354"+2
+//aloshfd
+martin=farias + 3 * function martin
+var break for while tzua lasdas function
